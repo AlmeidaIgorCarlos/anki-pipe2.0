fda60c2ff95c90dfc24b453a6b4eedbebff8933e
almeidaigorcarlos <almeidaigorcarlos@gmail.com>
2020-12-31 02:12:37 -0300

    Fixing eslint errors from index file

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import * as http from './server/server'
-import { HomeController } from './controllers/home-controller'
-import { NotFoundController } from './controllers/not-found-controller'
-import config from './server/config/config'
-import fs from 'fs'
+import * as http from './server/server';
+import { HomeController } from './controllers/home-controller';
+import { NotFoundController } from './controllers/not-found-controller';
+import config from './server/config/config';
+import fs from 'fs';
 
 const server = new http.Server({
-    key: fs.readFileSync(config.sslKey),
-    cert: fs.readFileSync(config.sslCert)
-})
+	key: fs.readFileSync(config.sslKey),
+	cert: fs.readFileSync(config.sslCert)
+});
 
 server.setRoutes([{
-    method: http.Methods.GET,
-    pathname: '/home/:id',
-    controller: new HomeController()
+	method: http.Methods.GET,
+	pathname: '/home/:id',
+	controller: new HomeController()
 }, {
-    method: http.Methods.ALL,
-    pathname: '(.*)',
-    controller: new NotFoundController()
-}])
+	method: http.Methods.ALL,
+	pathname: '(.*)',
+	controller: new NotFoundController()
+}]);
 
-server.listen(8443)
+server.listen(8443);
