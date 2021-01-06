292821a17bfc46970bbcbf37033ad3eaa05e9cd5
Bruno Mendes <bruno.mendes51@hotmail.com>
2021-01-05 23:22:39 -0300

    add repository on constructor

--- src/domain/sentence.ts
+++ src/domain/sentence.ts
@@ -1,25 +1,30 @@
 import { Card } from './card';
 import { Dictionary } from './dictionary';
+import Repository from './repository';
 
 export class Sentence {
     private readonly _sentence: string;
     private readonly _word: string;
     private readonly _dictionary: Dictionary
+    private readonly _repository: Repository
     
     /**
      * Constructor of the Sentence class
      * @param {string} sentence The main sentence of the card
      * @param {string} word The desired word that the user wants to search the meaning
      * @param {object} dictionary Instance of an implementation of the Dictionary interface
+     * @param {object} repository Instance of an implementation of the repository instance
      */
     public constructor(
     	sentence: string,
     	word: string,
-    	dictionary: Dictionary
+    	dictionary: Dictionary,
+    	repository: Repository
     ){
     	this._sentence = sentence;
     	this._word = word;
     	this._dictionary = dictionary;
+    	this._repository = repository;
     }
 
     /**
