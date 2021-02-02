c4144ebbd31eb1ba116bddbc78d769e9a94f0fb5
Bruno Mendes <bruno.mendes51@hotmail.com>
2021-02-01 21:13:50 -0300

    refatoring dialog component

--- views/scripts/dialog.js
+++ views/scripts/dialog.js
@@ -1,7 +1,17 @@
 function DialogControl () {
 
+	const dialog = document.getElementById('dialog');
 	const dialogButton1 = document.getElementById('dialog-button1');
 	const dialogButton2 = document.getElementById('dialog-button2');
+	const dialogTitle = document.getElementById('dialog-title');
+	const dialogBody = document.getElementById('dialog-body');
+	const dialogContainer = document.getElementById('dialog-container');
+
+	if (!dialog) {
+		alert('The dialog component is not present on the page.');
+		return;
+	}
+
 	dialogButton1.addEventListener('click', () => this.props.callbackButton1());
 	dialogButton2.addEventListener('click', () => this.props.callbackButton2());
 
@@ -17,17 +27,14 @@ function DialogControl () {
 	this.props = Object.assign({}, this.defaultProps),
 	this.setTitle = (text) => {
 		this.title = text;
-		const dialogTitle = document.getElementById('dialog-title');
 		dialogTitle.textContent = text;
 		this.props.title = text;
 	},
 	this.setMessage = (text) => {
-		const dialogBody = document.getElementById('dialog-body');
 		dialogBody.textContent = text;
 		this.props.message = text;
 	},
 	this.show = () => {
-		const dialogContainer = document.getElementById('dialog-container');
 		if (!dialogContainer) {
 			alert('The dialog component is not present on the page.');
 			return;
@@ -36,26 +43,22 @@ function DialogControl () {
 		dialogContainer.style.display = 'flex';
 	},
 	this.hide = () => {
-		const dialogContainer = document.getElementById('dialog-container');
 		dialogContainer.style.display = 'none';
 		Object.assign(this.props, this.defaultProps);
 	},
 	this.setButton1 = (title, action) => {
-		const dialogButton1 = document.getElementById('dialog-button1');
 		dialogButton1.textContent = title;
 		this.props.titleButton1 = title;
 		this.props.callbackButton1 = action;
-		this.initActionButtons();
+		this.initButton1();
 	},
 	this.setButton2 = (title, action) => {
-		const dialogButton2 = document.getElementById('dialog-button2');
 		dialogButton2.textContent = title;
 		this.props.titleButton2 = title;
 		this.props.callbackButton2 = action;
-		this.initActionButtons();
+		this.initButton2();
 	};
 	this.setWidth = (width) => {
-		const dialog = document.getElementById('dialog');
 		dialog.style.width = `${width}px`;
 		dialog.style.marginLeft = `-${width / 2}px`;
 		this.props.width = width;
@@ -68,8 +71,6 @@ function DialogControl () {
 		this.setWidth(this.props.width);
 	};
 	this.initDialogAnimation = () => {
-		const dialogContainer = document.getElementById('dialog-container');
-		const dialog = document.getElementById('dialog');
 		dialog.addEventListener('click', (e) => e.stopPropagation());
 		dialogContainer.addEventListener('click', (e)=> {
 			dialog.classList.remove('swing-animation');
@@ -80,15 +81,20 @@ function DialogControl () {
 		});
 	};
 	this.initActionButtons = () => {
+		this.initButton1();
+		this.initButton2();
+	};
+	this.initButton1 = () => {
 		if (this.props.titleButton1)
-			document.getElementById('dialog-button1').style.display = 'block';
+			document.getElementById('dialog-button1').style.visibility = 'visible';
 		else
-			document.getElementById('dialog-button1').style.display = 'none';
-
+			document.getElementById('dialog-button1').style.visibility = 'hidden';
+	};
+	this.initButton2 = () => {
 		if (this.props.titleButton2)
-			document.getElementById('dialog-button2').style.display = 'block';
+			document.getElementById('dialog-button2').style.visibility = 'visible';
 		else
-			document.getElementById('dialog-button2').style.display = 'none';
+			document.getElementById('dialog-button2').style.visibility = 'hidden';
 	};
 	this.init = () => {
 		this.initValues();
